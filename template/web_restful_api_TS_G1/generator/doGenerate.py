46c7269f0a8ce5e17aa8decce963c8e3ad6af76c
lucuicheng <lucuicheng@163.com>
2020-01-02 13:36:06 +0800

    Description:[Feature:000] 完善简单模板 Module: restful api Reviewer:@lucuichen

--- template/web_restful_api_TS_G1/generator/doGenerate.py
+++ template/web_restful_api_TS_G1/generator/doGenerate.py
@@ -1,6 +1,13 @@
 from lib import Utils
 
+content = Utils.template('BaseFullService', 'web_restful_api_TS_G1').render(name='info', alias='资讯')
+with open('../dist/' + 'InfoService' + '.ts', 'w') as fp:
+    fp.write(content)
 
-content = Utils.template('route', 'web_restful_api_TS_G1').render(name='test', alias='资讯')
-with open('../dist/' + 'route' + '.js', 'w') as fp:
+content = Utils.template('BasePartService', 'web_restful_api_TS_G1').render(name='info', alias='资讯')
+with open('../dist/' + 'userInfoService' + '.ts', 'w') as fp:
+    fp.write(content)
+
+content = Utils.template('route', 'web_restful_api_TS_G1').render(name='info', alias='资讯')
+with open('../dist/' + 'route_info' + '.ts', 'w') as fp:
     fp.write(content)
