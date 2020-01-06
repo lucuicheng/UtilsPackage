440a300b2b92ece0d71811ae8a112b73b027111f
lucuicheng <lucuicheng@163.com>
2020-01-06 15:08:30 +0800

    Description:[Feature:000] 修改 & 新增模板，归类到一个文件中 Module: restful api Reviewer:@lucuichen

--- template/web_restful_api_TS_G1/generator/doGenerate.py
+++ template/web_restful_api_TS_G1/generator/doGenerate.py
@@ -1,6 +1,6 @@
 from lib import Utils, Data
 
-target = [
+targets = [
     Data.Target('info', '资讯'),
     Data.Target('project', '项目'),
     Data.Target('activity', '活动'),
@@ -17,33 +17,36 @@ target = [
     Data.Target('identity', '认证'),
     Data.Target('comment', '评论'),
 ]
-for group in target:
-    content = Utils.template('BaseFullService', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
-    with open('../dist/' + group.name.capitalize() + 'Service' + '.ts', 'w') as fp:
-        fp.write(content)
-    #
-    # content = Utils.template('BasePartService', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
-    # with open('../dist/' + 'user_' + group.name.capitalize() + 'Service' + '.ts', 'w') as fp:
+for group in targets:
+    # content = Utils.template('BaseFullService', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
+    # with open('../dist/' + group.name.capitalize() + 'Service' + '.ts', 'w') as fp:
     #     fp.write(content)
-    #
-    # content = Utils.template('route', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
-    # with open('../dist/' + 'route_' + group.name + '.ts', 'w') as fp:
-    #     fp.write(content)
-    #
-    # #
     #
     # content = Utils.template('BaseFullTestCase', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
     # with open('../dist/test/' + group.name.capitalize() + 'ServiceTest' + '.ts', 'w') as fp:
     #     fp.write(content)
     #
-    # content = Utils.template('BasePartTestCase', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
-    # with open('../dist/test/' + 'user_' + group.name + 'ServiceTest.ts', 'w') as fp:
-    #     fp.write(content)
-
-    # content = Utils.template('UsedService', 'web_restful_api_TS_G1').render(name=group.name, alias=group.alias)
-    # with open('../dist/' + 'user_' + group.name + 'Service.ts', 'w') as fp:
-    #     fp.write(content)
 
     print('export { default as %sService } from \'./data/%sService\';' % (group.name, group.name.capitalize()))
     # print("import { %s, %sExtraDoc } from '@model/%s';" % (group.name.capitalize(), group.name.capitalize(), group.name.capitalize()))
     # print(group.name + 'Service,')
+
+content = Utils.template('BasePartService', 'web_restful_api_TS_G1').render(group=targets)
+with open('../dist/UserService.ts', 'w') as fp:
+    fp.write(content)
+
+content = Utils.template('route', 'web_restful_api_TS_G1').render(group=targets)
+with open('../dist/Routes.ts', 'w') as fp:
+    fp.write(content)
+
+content = Utils.template('UsedService', 'web_restful_api_TS_G1').render(group=targets)
+with open('../dist/UsedRoutes.ts', 'w') as fp:
+    fp.write(content)
+
+content = Utils.template('UsedUserService', 'web_restful_api_TS_G1').render(group=targets)
+with open('../dist/UsedUserRoutes.ts', 'w') as fp:
+    fp.write(content)
+
+# content = Utils.template('BasePartTestCase', 'web_restful_api_TS_G1').render(group=targets)
+# with open('../dist/test/UserServiceTest.ts', 'w') as fp:
+#     fp.write(content)
